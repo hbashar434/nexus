97d9b6d86b6bd3c582371344d94a56915937d3a6
hbashar434 <h.bashar434@gmail.com>
2023-09-16 13:13:07 +0600

    initial

---
+++ src/layout/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className=' text-violet-600 font-bold text-5xl'>Hello</h1>
+        </div>
+    );
+};
+
+export default Home;
